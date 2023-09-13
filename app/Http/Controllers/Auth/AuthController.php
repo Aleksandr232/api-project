0038c540afaa9000ece9a20dc239ec0457c95e50
Aleksandr232 <aleksrowing232@gmail.com>
2023-09-14 02:28:41 +0300

    auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json(['message' => 'Регистрация успешна'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            throw ValidationException::withMessages(['message' => 'Неверные учетные данные']);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+}
+
+
+
+
