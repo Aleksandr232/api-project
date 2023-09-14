3c8bfda7dd158ab4f802a7fa14bb3d6edbc1cc38
Aleksandr232 <aleksrowing232@gmail.com>
2023-09-15 02:24:10 +0300

    authController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -15,19 +15,30 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    /**
- * @OA\Post(
- *     path="api/register",
- *     tags={"App"},
- *     summary="Регистрация нового пользоваетля",
- *     description="Создает нового пользователя",
- *     @OA\Response(
- *         response=200,
- *         description="Успешная операция"
- *     ),
- * )
- *
-*/
+   /**
+     * @OA\Post(
+     *     path="api/register",
+     *     tags={"Auth"},
+     *     summary="Регистрация нового пользователя",
+     *     description="Создает нового пользователя",
+     *     @OA\RequestBody(
+     *         description="Детали нового пользователя",
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", example="John Doe"),
+     *             @OA\Property(property="email", type="string", format="email", example="johndoe@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="secret")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успешная операция",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="string", example="Регистрация прошла успешно")
+     *         )
+     *     ),
+     * )
+     */
 
     public function register(Request $request)
     {
@@ -43,6 +54,41 @@ class AuthController extends Controller
 
         return response()->json(['success'=>'Регистрация прошла успешно']);
     }
+
+
+    /**
+     * @OA\Post(
+     *     path="api/login",
+     *     tags={"Auth"},
+     *     summary="Аутентификация пользователя",
+     *     description="Аутентификация пользователя на основе email и password",
+     *     @OA\RequestBody(
+     *         description="Данные пользователя",
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="email", type="string", format="email", example="johndoe@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="secret")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успешная операция",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Успешно вошли в систему"),
+     *             @OA\Property(property="token", type="string", example="token"),
+     *             @OA\Property(property="access_code", type="string", example="access_code"),
+     *             @OA\Property(property="username", type="string", example="John Doe")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Ошибка аутентификации",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Ошибка аутентификации")
+     *         )
+     *     ),
+     * )
+     */
 
 
     public function login(Request $request)
@@ -70,6 +116,22 @@ class AuthController extends Controller
             return response()->json(['message' => 'Ошибка аутентификации'], 403);
         }
     }
+
+      /**
+     * @OA\Get(
+     *    path="api/logout",
+     *     tags={"Auth"},
+     *     summary="Выход из системы",
+     *     description="Выход текущего аутентифицированного пользователя из системы",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успешная операция",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="string", example="Пока! John Doe")
+     *         )
+     *     ),
+     * )
+     */
 
     public function logout(Request $request) {
 
