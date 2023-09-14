d28c3891059d5359b8bd8d07725fa4c0f6e4d49e
Aleksandr232 <aleksrowing232@gmail.com>
2023-09-14 19:12:31 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/* Route::group(['prefix' => 'v1'], function () {
+ Route::group(['prefix' => 'v1'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 });
- */
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+
+/* Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']); */
