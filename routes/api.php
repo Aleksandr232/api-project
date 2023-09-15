3ba0467d8f7578f5ec62101e39e9d573f2fe825b
Aleksandr232 <aleksrowing232@gmail.com>
2023-09-15 03:11:39 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,31 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
 // Запрещаем переход на другую страницу, если токен не прошел
-Route::middleware('auth:sanctum')->get('/dashboard', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/dashboard', function (Request $request) {
     if ($request->user()) {
         return response()->json(['message' => 'Добро пожаловать']);
+    } else {
+        abort(401, 'Unauthorized');
+    }
+}); */
+
+Route::middleware('auth:sanctum')->get('/dashboard', function (Request $request) {
+    $user = $request->user();
+
+    if (!empty($user->remember_token)) {
+        return response()->json(['message' => 'Добро пожаловать, ' . $user->name]);
+    } else {
+        abort(401, 'Unauthorized');
+    }
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $user = $request->user();
+
+    if (!empty($user->remember_token)) {
+        $user->remember_token = null;
+        $user->save();
+        return response()->json(['message' => 'Вы успешно вышли из системы']);
     } else {
         abort(401, 'Unauthorized');
     }
